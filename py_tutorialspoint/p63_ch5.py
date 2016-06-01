062ddb4b60b04bf774126f9e9ccf4719283a66f0
shyam <ssunder@salesforce.com>
2016-05-31 21:12:02 -0700

    added p63 & p76

--- py_tutorialspoint/p63_ch5.py
+++ py_tutorialspoint/p63_ch5.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+
+print "Arithmetic Operator"
 a = 21
 b = 10
 c = 0
@@ -23,7 +25,64 @@ b = 3
 c = a**b
 print "Line 6 - Value of c is ", c
 
-a = 10
-b = 5
+a = 11.0
+b = 5.0
 c = a//b
 print "Line 7 - Value of c is ", c
+
+print "Comparison Operator"
+
+x = 21
+y = 10
+z = 0
+
+if(x==y):
+    print "Line 1 - x is equal to y"
+else:
+    print "Line 2 - x is not equal to y"
+
+if(x<>y):
+    print "Line 1 - x is not equal to y"
+else:
+    print "Line 2 - x is equal to y"
+
+if(x!=y):
+    print "Line 1 - x is not equal to y"
+else:
+    print "Line 2 - x is equal to y"
+
+if(x<y):
+    print "Line 1 - x is less than y"
+else:
+    print "Line 2 - x is not less than y"
+
+if(x>y):
+    print "Line 1 - x is greater than y"
+else:
+    print "Line 2 - x is not greater than y"
+
+
+print "Membership operator in not in"
+
+name = "michael"
+names = ['michael', 'Laura', 'robin', 'singh']
+
+if('m' in name):
+    print "\'m\' is in name \'michael\'"
+else:
+    print "\'m\' not in \'michael\'"
+
+if('laura' in names):
+    print "laura is in names"
+else:
+    print "laura is not in names"
+
+print "Python identity operators- is / is not"
+
+xx = "test"
+yy = "fire"
+
+if(xx is yy):
+    print "xx is yy"
+else:
+    print "xx is not yy"

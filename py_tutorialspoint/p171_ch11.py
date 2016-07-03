2a245bbb6177c18cb6c7eac3292213de9e9004a2
shyam <ssunder@salesforce.com>
2016-07-03 12:49:01 -0700

    added p178_ch12.py

--- py_tutorialspoint/p171_ch11.py
+++ py_tutorialspoint/p171_ch11.py
@@ -5,3 +5,26 @@ tup2 = (1, 2, 3, 4, 5)
 tup3 = "a", "b", "c", "d"
 
 print "tup1 : \n", tup1
+tup1 = ("test",)
+print "tup1 : \n", tup1
+print type(tup1), type(tup2), type(tup3)
+
+print "tup2[1:] :- ", tup2[1:]
+
+tup3 = tup3 + tup2
+print "tup3 = tup3 + tup2 \n", tup3
+
+try:
+    tup3[0] = "changing"
+except(TypeError):
+    print "trying to update tuple --- NOT ALLOWED"
+
+try:
+    del tup1
+    print tup1
+except(NameError):
+    print "Name <tup1> not defined"
+except:
+    print "UNKNOWN error"
+
+print "Good Bye!!"
---
+++ py_tutorialspoint/p178_ch12.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+print "python dictionary"
+
+print "create empty dict using dict ={}"
+
+dict1 = {}
+print type(dict)
